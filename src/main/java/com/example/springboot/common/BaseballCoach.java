4ef1dcbfde157dd2c8e034c645abad13078e4fa0
quangbach1709 <bachh1124@gmail.com>
2024-08-28 15:40:25 +0700

    lazy

--- src/main/java/com/example/springboot/common/BaseballCoach.java
+++ src/main/java/com/example/springboot/common/BaseballCoach.java
@@ -1,8 +1,12 @@
 package com.example.springboot.common;
 
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Primary //Them do uu tien khi chay chuong trinh
+//@Lazy //Khi chay chuong trinh thi khong khoi tao bean nay ngay ma khi goi moi khoi tao
 public class BaseballCoach implements Coach {
     @Override
     public String getDailyWorkout() {

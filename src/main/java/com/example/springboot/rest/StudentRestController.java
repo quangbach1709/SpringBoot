0c57d1070ce9dfef5f32b35c29605c0d42212084
quangbach1709 <bachh1124@gmail.com>
2024-09-03 14:07:29 +0700

    Exception Handing xu ly ngoai le

--- src/main/java/com/example/springboot/rest/StudentRestController.java
+++ src/main/java/com/example/springboot/rest/StudentRestController.java
@@ -1,9 +1,8 @@
 package com.example.springboot.rest;
 
 import com.example.springboot.entity.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,15 +10,36 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-    //define endpoint for "/students" - return list of students
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        List<Student> theStudents = new ArrayList<>();
+    private List<Student> theStudents;
+
+    //define @PostConstruct to load the student data only once
+    @PostConstruct // chay sau khi constructor va truoc cac phuong thuc khac
+    public void loadData() {
+        theStudents = new ArrayList<>();
 
         theStudents.add(new Student("John", "Doe"));
         theStudents.add(new Student("Mary", "Public"));
         theStudents.add(new Student("Suzan", "Doe"));
+    }
+
+    //define endpoint for "/students" - return list of students
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+
 
         return theStudents;
     }
+
+    //define endpoint for "/students/{studentId}" - return student at index
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {//@PathVariable: lay gia tri tu URL
+
+        //check the studentId against list size
+        if ((studentId >= theStudents.size()) || (studentId < 0)) {
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
+        return theStudents.get(studentId);
+    }
+
+
 }

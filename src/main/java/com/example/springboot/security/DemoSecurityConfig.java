3c95c26ad4fdf38921df28ff102c19c1a18eeb1b
quangbach1709 <bachh1124@gmail.com>
2024-09-04 15:23:41 +0700

    restrict access based on roles

--- src/main/java/com/example/springboot/security/DemoSecurityConfig.java
+++ src/main/java/com/example/springboot/security/DemoSecurityConfig.java
@@ -2,9 +2,13 @@ package com.example.springboot.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration //dung de xac dinh day la class cau hinh
 public class DemoSecurityConfig {
@@ -27,5 +31,25 @@ public class DemoSecurityConfig {
                 .roles("ADMIN", "MANAGER", "EMPLOYEE")
                 .build();
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {//cau hinh cac quyen truy cap cho cac user
+        httpSecurity.authorizeHttpRequests(
+                configurer -> configurer
+                        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/employees/").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+        );
+
+        //use httpBasic authentication
+        httpSecurity.httpBasic(Customizer.withDefaults());//su dung httpBasic authentication de xac thuc user khi truy cap vao cac resource
+
+        //disable cross site request forgery (CSRF)
+        httpSecurity.csrf(csrf -> csrf.disable());//tat chuc nang CSRF de don gian hoa viec test
+
+        return httpSecurity.build();
     }
 }

b06927eb95c1614349079e24189bcdf525f4ac34
quangbach1709 <bachh1124@gmail.com>
2024-09-04 22:06:46 +0700

    JDBC authentication

--- src/main/java/com/example/springboot/security/DemoSecurityConfig.java
+++ src/main/java/com/example/springboot/security/DemoSecurityConfig.java
@@ -8,30 +8,40 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration //dung de xac dinh day la class cau hinh
 public class DemoSecurityConfig {
+    //    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager() {//tao ra cac user trong bo nho de su dung
+//        UserDetails john = User.builder()
+//                .username("john")
+//                .password("{noop}123")
+//                .roles("EMPLOYEE")
+//                .build();
+//        UserDetails mary = User.builder()
+//                .username("mary")
+//                .password("{noop}123")
+//                .roles("MANAGER", "EMPLOYEE")
+//                .build();
+//
+//        UserDetails susan = User.builder()
+//                .username("susan")
+//                .password("{noop}123")
+//                .roles("ADMIN", "MANAGER", "EMPLOYEE")
+//                .build();
+//        return new InMemoryUserDetailsManager(john, mary, susan);
+//    }
+    //add support for jdbc ... no more in-memory users
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {//tao ra cac user trong bo nho de su dung
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}123")
-                .roles("EMPLOYEE")
-                .build();
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}123")
-                .roles("MANAGER", "EMPLOYEE")
-                .build();
-
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}123")
-                .roles("ADMIN", "MANAGER", "EMPLOYEE")
-                .build();
-        return new InMemoryUserDetailsManager(john, mary, susan);
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {//tao ra cac user trong database de su dung thay vi luu trong bo nho nhu tren (InMemoryUserDetailsManager)
+        return new JdbcUserDetailsManager(dataSource);//su dung JdbcUserDetailsManager de quan ly cac user trong database thay vi luu trong bo nho
     }
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {//cau hinh cac quyen truy cap cho cac user

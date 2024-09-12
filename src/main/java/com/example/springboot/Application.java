b44c2d39f9d32206d57dbea5a1c9c6071a13fc3c
quangbach1709 <bachh1124@gmail.com>
2024-09-12 22:10:21 +0700

    Spring MVC with Thymeleaf

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -1,12 +1,9 @@
 package com.example.springboot;
 
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import java.util.List;
 
 //@SpringBootApplication(scanBasePackages = {"com.example.springboot", "util"}) // quet cac tep ngoai pham vi
 @SpringBootApplication

3b051fd6f8700c68846773be11e464392a5a3ccf
quangbach1709 <bachh1124@gmail.com>
2024-09-17 17:01:27 +0700

    Spring MVC Form Data Binding - Drop-Down Lists

--- src/main/java/com/example/springboot/controller/StudentController.java
+++ src/main/java/com/example/springboot/controller/StudentController.java
@@ -1,14 +1,20 @@
 package com.example.springboot.controller;
 
 import com.example.springboot.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
 
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel) {
@@ -17,6 +23,10 @@ public class StudentController {
 
         //add student object to the model
         theModel.addAttribute("student", new Student());
+
+        // add the country options to the model
+        theModel.addAttribute("countries", countries);
+
         return "student-form";
     }
 

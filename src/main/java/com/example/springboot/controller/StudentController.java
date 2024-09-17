76431e5db08de6898b6091752e3fa42f234d8b0e
quangbach1709 <bachh1124@gmail.com>
2024-09-17 17:39:15 +0700

    Spring MVC Form Data Binding - Radio Buttons

--- src/main/java/com/example/springboot/controller/StudentController.java
+++ src/main/java/com/example/springboot/controller/StudentController.java
@@ -16,6 +16,9 @@ public class StudentController {
     @Value("${countries}")
     private List<String> countries;
 
+    @Value("${languages}")
+    private List<String> languages;
+
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel) {
 
@@ -26,6 +29,9 @@ public class StudentController {
 
         // add the country options to the model
         theModel.addAttribute("countries", countries);
+
+        //add the language options to the model
+        theModel.addAttribute("languages", languages);
 
         return "student-form";
     }

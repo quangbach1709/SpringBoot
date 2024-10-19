3d49f286fb1db95427ba1ecad622dfaa775acabc
quangbach1709 <bachh1124@gmail.com>
2024-10-19 16:38:34 +0700

    Spring MVC validation String for int fields and custom messages

--- src/main/java/com/example/springboot/Customer.java
+++ src/main/java/com/example/springboot/Customer.java
@@ -8,9 +8,10 @@ public class Customer {
     @NotNull(message = "Last name is required") // kiem tra xem co rong hay khong
     @Size(min = 2, message = "Last name must be at least 2 characters long")// kiem tra do dai cua chuoi nhap vao
     private String lastName = "";
+    @NotNull(message = "is required") // kiem tra xem co rong hay khong
     @Min(value = 0, message = "must be greater than or equal to zero") // kiem tra so luong phai lon hon hoac bang 0
     @Max(value = 10, message = "must be less than or equal to 10")  // kiem tra so luong phai nho hon hoac bang 10
-    private int freePasses;
+    private Integer freePasses;
     // kiem tra xem co dung dinh dạng hay không (5 ký tự hoặc 5 số)
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
     private String postalCode;
@@ -32,11 +33,11 @@ public class Customer {
     }
 
 
-    public int getFreePasses() {
+    public Integer getFreePasses() {
         return freePasses;
     }
 
-    public void setFreePasses(int freePasses) {
+    public void setFreePasses(Integer freePasses) {
         this.freePasses = freePasses;
     }
 

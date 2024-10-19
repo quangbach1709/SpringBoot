72d9929bc6ff742a1490e0145cc8f90eb747a05e
quangbach1709 <bachh1124@gmail.com>
2024-10-19 16:25:54 +0700

    Spring MVC validation applying regular exprssions

--- src/main/java/com/example/springboot/Customer.java
+++ src/main/java/com/example/springboot/Customer.java
@@ -1,9 +1,6 @@
 package com.example.springboot;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
 
@@ -14,6 +11,9 @@ public class Customer {
     @Min(value = 0, message = "must be greater than or equal to zero") // kiem tra so luong phai lon hon hoac bang 0
     @Max(value = 10, message = "must be less than or equal to 10")  // kiem tra so luong phai nho hon hoac bang 10
     private int freePasses;
+    // kiem tra xem co dung dinh dạng hay không (5 ký tự hoặc 5 số)
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
 
     public String getFirstName() {
         return firstName;
@@ -36,7 +36,15 @@ public class Customer {
         return freePasses;
     }
 
-    public void setFreePasses(@Min(value = 0, message = "must be greater than or equal to zero") @Max(value = 10, message = "must be less than or equal to 10") int freePasses) {
+    public void setFreePasses(int freePasses) {
         this.freePasses = freePasses;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
     }
 }

80a6a43b4252a51fb47dc77e5d32ca627039e2a4
quangbach1709 <bachh1124@gmail.com>
2024-10-19 14:27:42 +0700

    Spring MVC validation @initBinder

--- src/main/java/com/example/springboot/CustomerController.java
+++ src/main/java/com/example/springboot/CustomerController.java
@@ -2,15 +2,25 @@ package com.example.springboot;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    // xu ly truong hop nhap vao la chuoi rong thi se duoc xem nhu la null
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);// true la xoa het khoang trang
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);// xoa het khoang trang
+    }
 
     @GetMapping("/")
     public String showForm(Model model) {
@@ -21,6 +31,7 @@ public class CustomerController {
     // xu ly form khi submit
     @PostMapping("/processForm")
     public String processForm(@Valid @ModelAttribute("customer") Customer customer, BindingResult bindingResult) {
+        System.out.println("Last name: |" + customer.getLastName() + "|");
         //lay ra doi tuong customer tu form va kiem tra xem co loi hay khong
         if (bindingResult.hasErrors()) {
             return "customer-form";

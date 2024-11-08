d0914dacf0c582556319548c7b77fad1ba6510f8
quangbach1709 <bachh1124@gmail.com>
2024-11-08 16:26:48 +0700

    Spring MVC CRUD

---
+++ src/main/java/com/example/springboot/dao/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.example.springboot.dao;
+
+
+import com.example.springboot.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // that's it ... no need to write any code LOL!
+
+}

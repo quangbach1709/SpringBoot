38776b5c4cc04f002f45cec5d44c70b4dce1ab99
quangbach1709 <bachh1124@gmail.com>
2024-11-09 14:28:12 +0700

    Spring MVC CRUD Add Employees

--- src/main/java/com/example/springboot/controller/EmployeeController.java
+++ src/main/java/com/example/springboot/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import com.example.springboot.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -26,6 +28,20 @@ public class EmployeeController {
 
         //add to the spring model
         model.addAttribute("employees", listEmployees);
-        return "list-employees";
+        return "employees/list-employees";
     }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model) {
+        Employee theEmployee = new Employee();
+        model.addAttribute("employee", theEmployee);
+        return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+        employeeService.save(theEmployee);
+        return "redirect:/employees/list";
+    }
+
 }
